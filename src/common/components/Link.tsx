df0940533197a42cb7ff9452709226d4aebacf03
Andrés Loreto <andresantigoloretoaguero@gmail.com>
2022-03-16 23:39:13 -0400

    Removed role destructuring from Link

--- src/common/components/Link.tsx
+++ src/common/components/Link.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
-import clsx from 'clsx';
-import { useRouter } from 'next/router';
-import NextLink, { LinkProps as NextLinkProps } from 'next/link';
+
 import MuiLink, { LinkProps as MuiLinkProps } from '@mui/material/Link';
 import { styled } from '@mui/material/styles';
+import clsx from 'clsx';
+import NextLink, { LinkProps as NextLinkProps } from 'next/link';
+import { useRouter } from 'next/router';
 
 // Add support for the sx prop for consistency with the other branches.
 const Anchor = styled('a')({});
@@ -64,7 +65,6 @@ const Link = React.forwardRef<HTMLAnchorElement, LinkProps>(function Link(
 		noLinkStyle,
 		prefetch,
 		replace,
-		role, // Link don't have roles.
 		scroll,
 		shallow,
 		...other

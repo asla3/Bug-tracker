e522f80bc748d1ed8102e5635c7ac75a574d4848
Andrés Loreto <andresantigoloretoaguero@gmail.com>
2022-06-09 14:27:43 -0400

    Autocompleted will render a TextField by default if not given a renderInput

--- src/common/components/Autocomplete/Autocomplete.tsx
+++ src/common/components/Autocomplete/Autocomplete.tsx
@@ -1,20 +1,22 @@
 import * as React from 'react';
 
-import type { AutocompleteValue as MuiAutocompleteValue } from '@mui/base/AutocompleteUnstyled';
+import type { AutocompleteFreeSoloValueMapping as MuiAutocompleteFreeSoloValueMapping } from '@mui/base/AutocompleteUnstyled';
 import MuiAutocomplete, {
 	AutocompleteProps as MuiAutocompleteProps,
 	AutocompleteRenderInputParams as MuiAutocompleteRenderInputParams,
 } from '@mui/material/Autocomplete';
+import TextField, { TextFieldProps } from '@mui/material/TextField';
 import { unstable_useControlled as useControlled } from '@mui/utils';
 
 import callAll from '@/common/utils/callAll';
 
-interface AutocompleteProps<
+//	todo overwrite MUI's `value` prop so it can accept `null` as value when `disableClearable` is true.
+interface AutocompleteBaseProps<
 	T,
-	Multiple extends boolean | undefined = undefined,
-	DisableClearable extends boolean | undefined = undefined,
-	FreeSolo extends boolean | undefined = undefined,
-	ChipComponent extends React.ElementType<any> = 'div'
+	Multiple extends boolean | undefined,
+	DisableClearable extends boolean | undefined,
+	FreeSolo extends boolean | undefined,
+	ChipComponent extends React.ElementType<any>
 > extends Omit<
 		MuiAutocompleteProps<
 			T,
@@ -24,19 +26,80 @@ interface AutocompleteProps<
 			ChipComponent
 		>,
 		'renderInput'
+	> {}
+
+/* 
+	defining our own AutocompleteValue because mui's says that the value can't be `null`
+	when `disableClearable` is `true`, even though the initial value for an uncontrolled `Autocomplete`
+	when `multiple` is `false` will always be `null`.
+*/
+type AutocompleteValue<
+	T,
+	Multiple extends boolean | undefined,
+	FreeSolo extends boolean | undefined
+> = Multiple extends undefined | false
+	? T | null | MuiAutocompleteFreeSoloValueMapping<FreeSolo>
+	: Array<T | MuiAutocompleteFreeSoloValueMapping<FreeSolo>>;
+
+interface AutocompleteWithRenderInputProps<
+	T,
+	Multiple extends boolean | undefined,
+	DisableClearable extends boolean | undefined,
+	FreeSolo extends boolean | undefined,
+	ChipComponent extends React.ElementType<any>
+> extends AutocompleteBaseProps<
+		T,
+		Multiple,
+		DisableClearable,
+		FreeSolo,
+		ChipComponent
 	> {
-	/**
-	 * Renders the input.
-	 */
 	renderInput: (
 		params: MuiAutocompleteRenderInputParams,
-		value: MuiAutocompleteValue<T, Multiple, DisableClearable, FreeSolo> | null // Have to add `null` because MuiAutocompleteValue only returns a union that contains `null` when `disableClearable` is `false`.
+		value: AutocompleteValue<T, Multiple, FreeSolo>
 	) => React.ReactNode;
+	inputProps?: never;
 }
 
-/**
- * Extended MUI `<Autocomplete />`.
- */
+interface AutocompleteWithoutRenderInputProps<
+	T,
+	Multiple extends boolean | undefined,
+	DisableClearable extends boolean | undefined,
+	FreeSolo extends boolean | undefined,
+	ChipComponent extends React.ElementType<any>
+> extends AutocompleteBaseProps<
+		T,
+		Multiple,
+		DisableClearable,
+		FreeSolo,
+		ChipComponent
+	> {
+	inputProps?: Omit<TextFieldProps, keyof MuiAutocompleteRenderInputParams>;
+	renderInput?: never;
+}
+
+type AutocompleteProps<
+	T,
+	Multiple extends boolean | undefined = undefined,
+	DisableClearable extends boolean | undefined = undefined,
+	FreeSolo extends boolean | undefined = undefined,
+	ChipComponent extends React.ElementType<any> = 'div'
+> =
+	| AutocompleteWithRenderInputProps<
+			T,
+			Multiple,
+			DisableClearable,
+			FreeSolo,
+			ChipComponent
+	  >
+	| AutocompleteWithoutRenderInputProps<
+			T,
+			Multiple,
+			DisableClearable,
+			FreeSolo,
+			ChipComponent
+	  >;
+
 const Autocomplete = <
 	T,
 	Multiple extends boolean | undefined = undefined,
@@ -50,9 +113,10 @@ const Autocomplete = <
 	onChange,
 	// @ts-ignore
 	defaultValue = multiple ? [] : null,
-	// Assign same values to `Autocomplete`s with multiple selections to keep consistency. If not, fallback to theme default
+	// Assign same values to `Autocomplete`s with multiple selections to keep consistency.
 	disableCloseOnSelect = multiple || undefined,
 	filterSelectedOptions = multiple || undefined,
+	inputProps,
 	...props
 }: AutocompleteProps<
 	T,
@@ -81,7 +145,13 @@ const Autocomplete = <
 		DisableClearable,
 		FreeSolo,
 		ChipComponent
-	>['renderInput'] = (params) => renderInputProp(params, value);
+	>['renderInput'] = (params) => {
+		if (renderInputProp) {
+			return renderInputProp(params, value);
+		}
+
+		return <TextField {...params} {...inputProps} />;
+	};
 
 	return (
 		<MuiAutocomplete
@@ -98,6 +168,7 @@ const Autocomplete = <
 
 export * from '@mui/material/Autocomplete';
 
-export type { AutocompleteProps };
+//overwrite exports from mui
+export type { AutocompleteProps, AutocompleteValue };
 
 export default Autocomplete;

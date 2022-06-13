d6c1d8382fcc237ef0e3ec116eac8783ed232241
Andrés Loreto <andresantigoloretoaguero@gmail.com>
2022-06-13 17:41:02 -0400

    Removed fragments and structure of queries

--- src/graphql/mutations.ts
+++ src/graphql/mutations.ts
@@ -1,27 +1,17 @@
 import { gql } from '@/common/utils/graphqlRequestUtils';
 
-import { CORE_AUTH_USER_FIELDS_FRAGMENT } from './fragments';
-
 export const LOGIN_MUTATION = gql`
-	${CORE_AUTH_USER_FIELDS_FRAGMENT}
 	mutation Login($email: String!, $password: String!) {
 		login(email: $email, password: $password) {
-			token
-			user {
-				...CoreAuthUserFields
-			}
+			temp
 		}
 	}
 `;
 
 export const REGISTER_MUTATION = gql`
-	${CORE_AUTH_USER_FIELDS_FRAGMENT}
 	mutation Register($email: String!, $password: String!) {
 		register(email: $email, password: $password) {
-			token
-			user {
-				...CoreAuthUserFields
-			}
+			temp
 		}
 	}
 `;

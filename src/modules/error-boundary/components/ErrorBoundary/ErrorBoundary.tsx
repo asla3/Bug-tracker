d9ef479cb509f25b0b65949a3b8aa2409f6a1c7f
Andrés Loreto <39864434+asla3@users.noreply.github.com>
2022-06-21 18:14:47 +0000

    ErrorBoundary can now reset react-query errors

--- src/modules/error-boundary/components/ErrorBoundary/ErrorBoundary.tsx
+++ src/modules/error-boundary/components/ErrorBoundary/ErrorBoundary.tsx
@@ -3,6 +3,9 @@ import {
 	ErrorBoundary as REB_ErrorBoundary,
 	ErrorBoundaryPropsWithComponent as REB_ErrorBoundaryPropsWithComponent,
 } from 'react-error-boundary';
+import { useQueryErrorResetBoundary } from 'react-query';
+
+import callAll from '@/common/utils/callAll';
 
 import ErrorFallback from '../ErrorFallback';
 
@@ -27,13 +30,19 @@ const reportErrorToSentry = (
 const ErrorBoundary = ({
 	FallbackComponent,
 	onError,
+	onReset,
 	...props
-}: ErrorBoundaryProps) => (
-	<REB_ErrorBoundary
-		FallbackComponent={FallbackComponent || ErrorFallback}
-		onError={onError || reportErrorToSentry} // todo maybe we don't want `reportErrorToSentry` to get overriden. I'm considering letting `onError` get acess to the scope so it can append more info to it
-		{...props}
-	/>
-);
+}: ErrorBoundaryProps) => {
+	const { reset } = useQueryErrorResetBoundary();
+
+	return (
+		<REB_ErrorBoundary
+			FallbackComponent={FallbackComponent || ErrorFallback}
+			onError={onError || reportErrorToSentry} // todo maybe we don't want `reportErrorToSentry` to get overriden. I'm considering letting `onError` get acess to the scope so it can append more info to it
+			onReset={callAll(reset, onReset)}
+			{...props}
+		/>
+	);
+};
 
 export default ErrorBoundary;
